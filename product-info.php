3a6062b35a91f55d6d0b82617b5926589cf1d65c
nksah3090 <nksah3090@gmail.com>
2023-07-22 16:42:39 +0530

    done

--- product-info.php
+++ product-info.php
@@ -68,7 +68,7 @@
         <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="grocery.html">Grocery</a>
